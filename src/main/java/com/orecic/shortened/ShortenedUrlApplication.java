d8c37d8a34ad9d8d42fd4f26d7d28d2c8f34f5ea
Cicero Monteiro <cicerolmonteiro@gmail.com>
2021-07-09 17:02:33 -0300

    Initial commit

---
+++ src/main/java/com/orecic/shortened/ShortenedUrlApplication.java
@@ -0,0 +1,13 @@
+package com.orecic.shortened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenedUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenedUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orecic/shortened/ShortenedUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orecic.shortened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenedUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
